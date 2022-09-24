c3e589b27798aeac51f2029f80127de89651c2da
mpwhite2 <110784371+mpwhite2@users.noreply.github.com>
2022-09-24 08:41:28 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,67 @@
+input.onButtonPressed(Button.A, function () {
+    player.change(LedSpriteProperty.X, -1)
+})
 function blaster () {
+    pew = 1
     pewpew = game.createSprite(enemy.get(LedSpriteProperty.X), 0)
     for (let index = 0; index < 4; index++) {
         pewpew.change(LedSpriteProperty.Y, 1)
         basic.pause(140)
     }
+    if (pewpew.isTouching(player)){
+        game.gameOver()
+    }
+    pew = 0
+    pewpew.delete()
 }
+input.onButtonPressed(Button.AB, function () {
+    blast = 1
+    laser = game.createSprite(player.get(LedSpriteProperty.X), 4)
+    for (let index = 0; index < 4; index++) {
+        laser.change(LedSpriteProperty.Y, -1)
+        basic.pause(140)
+    }
+    if (laser.isTouching(enemy)){
+        enemy.delete()
+    }
+    blast = 0
+    laser.delete()
+})
+input.onButtonPressed(Button.B, function () {
+    player.change(LedSpriteProperty.X, 1)
+})
+let laser: game.LedSprite = null
+let blast = 0
 let pewpew: game.LedSprite = null
+let pew = 0
 let enemy: game.LedSprite = null
-let player = game.createSprite(2, 4)
+let player: game.LedSprite = null
+player = game.createSprite(2, 4)
 enemy = game.createSprite(0, 0)
 basic.forever(function () {
     if (enemy.get(LedSpriteProperty.X) == player.get(LedSpriteProperty.X)) {
-        basic.pause (randint(100,150))
+        basic.pause(randint(50, 1000))
         blaster()
     }
     if (enemy.get(LedSpriteProperty.X) < player.get(LedSpriteProperty.X)) {
-       basic.pause (randint(300,400))
+        basic.pause(randint(50, 1000))
         enemy.change(LedSpriteProperty.X, 1)
     }
-    if (enemy.get(LedSpriteProperty.X)>player.get(LedSpriteProperty.X)){
-        enemy.change(LedSpriteProperty.X,-1)
+    if (enemy.get(LedSpriteProperty.X) > player.get(LedSpriteProperty.X)) {
+        enemy.change(LedSpriteProperty.X, -1)
+        basic.pause(randint(50, 1000))
+    }
+    if (pew == 1 && blast == 1) {
+        if (pewpew.isTouching(laser)) {
+            pewpew.delete()
+            laser.delete()
+            basic.showLeds(`
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                `,100)
+        }
     }
 })
